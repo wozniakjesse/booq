84ac3642e313ce149897727b04e135d400750efe
Jesse Wozniak <wozniak.jesse@gmail.com>
2019-08-04 17:37:55 -0400

    homepage introduction

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,22 @@
-/* https://pawelgrzybek.github.io/siema/ */
-new Siema();
+(function() {
+    const SLIDER_INTERVAL = 7000;
+
+    /* https://pawelgrzybek.github.io/siema/ */
+    let carousel = new Siema({
+        loop: true
+    });
+
+    setInterval(function() {
+        carousel.next();
+    }, SLIDER_INTERVAL);
+    
+    let prevBtn = document.getElementById('carousel-prev');
+    let nextBtn = document.getElementById('carousel-next');
+    
+    prevBtn.addEventListener('click', function() {
+        carousel.prev();
+    });
+    nextBtn.addEventListener('click', function() {
+        carousel.next();
+    });
+})();
