8a1dd9bd65244d24062071b59acdec1c84718a1e
Jesse Wozniak <wozniak.jesse@gmail.com>
2019-08-04 20:51:31 -0400

    footer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,6 @@
-(function() {
-    const SLIDER_INTERVAL = 7000;
+const SLIDER_INTERVAL = 7000;
 
+if (document.getElementsByClassName('siema').length > 0) {
     /* https://pawelgrzybek.github.io/siema/ */
     let carousel = new Siema({
         loop: true
@@ -9,14 +9,28 @@
     setInterval(function() {
         carousel.next();
     }, SLIDER_INTERVAL);
-    
+
     let prevBtn = document.getElementById('carousel-prev');
     let nextBtn = document.getElementById('carousel-next');
-    
+
     prevBtn.addEventListener('click', function() {
         carousel.prev();
     });
     nextBtn.addEventListener('click', function() {
         carousel.next();
     });
-})();
+}
+
+/*
+*   Ridiculous fancy divider
+*/
+let dividers = document.getElementsByClassName('fancy-divider');
+for (let i = 0; i < dividers.length; ++i) {
+    for (let j = 0; j < 7; ++j) {
+        let dollaSign = document.createElement('span');
+        dollaSign.textContent = "$";
+        dividers[i].appendChild(dollaSign);
+    }
+}
+
+    
