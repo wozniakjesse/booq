bedd7152bfb7b71dc54548b2d5f15e3afd60d7fe
Jesse Wozniak <wozniak.jesse@gmail.com>
2019-11-17 11:49:40 -0500

    bookings - don't require cats

--- services/BookingService.js
+++ services/BookingService.js
@@ -39,9 +39,10 @@ module.exports = function(db, moment) {
 
     function insertBooking(res, req, db, complete){
         const classIds = req.body.class_id;
+        const catId = req.body.cat_id.length ? req.body.cat_id : null;
         var sql = "INSERT INTO bookings (room_id, cat_id, date_in, date_out, "
             + "hot_dogs, pancakes, user_id) VALUES (?,?,?,?,?,?,?) "
-        var inserts = [req.body.room_id, req.body.cat_id, req.body.date_in, 
+        var inserts = [req.body.room_id, catId, req.body.date_in, 
             req.body.date_out, req.body.hot_dogs, req.body.pancakes, req.user.id];
         sql = db.query(sql,inserts,function(error, results, fields){
             if(error){
